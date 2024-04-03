2c04762cda39760887ffd397a6930f80339f03e0
Ian Clark <idclark13@gmail.com>
2024-04-03 10:59:06 -0400

    initial test with port randomization passes

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,6 +1,30 @@
-use zero2prod::main;
+use std::{fmt::format, net::TcpListener};
 
-#[test]
-fn dummy_test() {
-    main()
+use zero2prod::run;
+
+#[tokio::test]
+async fn health_check_works() {
+    // Get started
+    let address = spawn_app();
+    let client = reqwest::Client::new();
+
+    // Act on it
+    let response = client
+        .get(&format!("{}/health_check", &address))
+        .send()
+        .await
+        .expect("Failed to execute request. ");
+
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
+
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind to address");
+    // get the port assigned by the OS
+    let port = listener.local_addr().unwrap().port();
+    let server = run(listener).expect("Failed to bind to address");
+    let _ = tokio::spawn(server);
+
+    format!("http://127.0.0.1:{}", port)
 }
