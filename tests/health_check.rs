a047b359e19b48edad24dd8cbf4a3cb88cd34723
Ian Clark <idclark13@gmail.com>
2024-04-12 09:06:03 -0400

    midway through pgpool refactor

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,6 +1,10 @@
 use std::{fmt::format, net::TcpListener};
 
-use zero2prod::startup::run;
+use sqlx::{Connection, PgConnection};
+use zero2prod::{
+    configuration::{self, get_configuration},
+    startup::run,
+};
 
 fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind to address");
@@ -33,6 +37,12 @@ async fn health_check_works() {
 async fn subscribe_returns_a_200_for_valid_form_data() {
     // Arrange
     let app_address = spawn_app();
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let connection_string = configuration.database.connection_string();
+    let mut connection = PgConnection::connect(&connection_string)
+        .await
+        .expect("Failed to connect to Postgres.");
+
     let client = reqwest::Client::new();
 
     // Act
@@ -46,6 +56,14 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
         .expect("Failed to execute requesst.");
 
     assert_eq!(200, response.status().as_u16());
+
+    let saved = sqlx::query!("SELECT email, name FROM subscriptions")
+        .fetch_one(&mut connection)
+        .await
+        .expect("Failed to fetch saved subscriptions");
+
+    assert_eq!(saved.email, "ursula_le_guin@gmail.com");
+    assert_eq!(saved.name, "le guin");
 }
 
 #[tokio::test]
